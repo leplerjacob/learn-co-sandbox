842b1210837c161310373542290f57e1a94c3965
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-11 22:53:47 +0000

    Automatically backed up by Learn

---
+++ case_statement.rb
@@ -0,0 +1,30 @@
+# name = "Alice"
+# name = "The White Rabbit"
+# name = "The Mad Hatter"
+
+# case name
+
+#   when "Alice"
+#     puts "Hello, Alice!"
+#   when "The White Rabbit"
+#     puts "Don't be late, White Rabbit"
+#   when "The Mad Hatter"
+#     puts "Welcome to the tea party, Mad Hatter"
+#   when "The Queen of Hearts"
+#     puts "Please don't chop of my head!"
+#   else 
+#     puts "Whoooo are you?"
+# end
+
+
+# # Method 1
+
+# def dry_method
+#   i = 0
+#   while i < 5 do
+#     i += 1
+#     puts "Hello there"
+#   end
+# end
+  
+# dry_method
---
+++ collection_data_types_introduction_notes.rb
@@ -0,0 +1,11 @@
+# new_array = ["hi", 9, 3.4, ['another array', 7, 9]]
+# puts new_array
+
+english_bands_by_city = {
+  :liverpool => "The Beatles",
+  :manchester => "The Smiths",
+  :coventry => "Delia Derbyshire and the BBC Radiophoncis Band",
+  :london => "Ziggy Stardust and the Spiders from Mars"
+}
+
+puts english_bands_by_city
